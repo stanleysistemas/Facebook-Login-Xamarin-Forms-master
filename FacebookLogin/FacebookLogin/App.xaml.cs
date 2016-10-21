5440e01a2df2ecf5e2f52d168c4883754ed4968e
stanley <stanley@MAQ_TI08>
2016-10-21 13:37:34 -0200

    Add project files.

---
+++ FacebookLogin/FacebookLogin/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FacebookLogin.Views;
+using Xamarin.Forms;
+
+namespace FacebookLogin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                Title = "Facebook Login"
+            };
+            //MainPage = new NavigationPage(new MainCsPage())
+            //{
+            //    Title = "Facebook Login"
+            //};
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
